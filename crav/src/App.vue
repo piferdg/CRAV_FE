0d46d5ee53d70ed67e11569975f81312f32963d2
Danny Pifer <piferdg@gmail.com>
2018-08-14 11:10:30 -0600

    Readding vue crav files without eslint

---
+++ crav/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    // Our app components here
+  },
+
+  data() {
+    return {
+      // this is our 'state', app data
+      apiUrl: '',
+      currentGenre: '',
+      genreList: [],
+      queriedResturants: [],
+      queriedFoods: [],
+      resturantList: [],
+      foodList: [],
+    };
+  },
+
+  methods: {
+    populateGenre() {
+      this.genreList = ['Italian', 'American', 'Mexican'];
+    },
+    populateFoods() {
+      this.foodList = ['tacos', 'hamburgers', 'pizza', 'burritos'];
+    },
+    populateResturants() {
+      this.resturantList = ['Taco Bell', 'McDonalds', 'Fazolis', 'Piccolos', 'Pizza Hut'];
+    },
+    getResturantByGenre() {
+
+    },
+    getFoodByGenre() {
+
+    },
+    getFoodByResturant() {
+
+    },
+  },
+
+  mounted() {
+    this.populateGenre();
+    this.populateFoods();
+    this.populateResturants();
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
